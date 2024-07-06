0300d48519d14eae54ee2f328b19c00a47fc798b
Nikhila-Rajani <146221009+Nikhila-Rajani@users.noreply.github.com>
2024-07-06 09:18:46 +0530

    final

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../model/userModel')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback"
+    callbackURL: "https://nikkz.online/google/callback"
 },
     async (token, tokenSecret, profile, done) => {
         try {
